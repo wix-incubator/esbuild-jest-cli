c030929cd725c54c07d6fe69dc65345cbbec0843
Yaroslav Serhieiev <yaroslavs@wix.com>
2023-04-14 11:58:32 +0300

    feat: expose programmatic api (#2)

--- build.mjs
+++ build.mjs
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 
+import { cosmiconfig } from 'cosmiconfig';
 import { build } from './index.mjs';
 
 try {
-  await build();
+  const explorer = cosmiconfig('esbuild-jest');
+  const esbuildJestBaseConfig = await explorer.search();
+  await build(esbuildJestBaseConfig.config);
 } catch (e) {
-  console.error(`${e}`);
+  console.error(`${e.stack || e}`);
   process.exit(1);
 }
