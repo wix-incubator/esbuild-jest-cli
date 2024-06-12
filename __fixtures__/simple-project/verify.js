62c7430cda6709d9de38a1969b728808ed49b4e7
Yaroslav Serhieiev <noomorph@gmail.com>
2024-06-12 15:41:23 +0300

    fix: ../../node_modules redirection

--- __fixtures__/simple-project/verify.js
+++ __fixtures__/simple-project/verify.js
@@ -22,8 +22,8 @@ function verifyDirectoryStructure() {
   assertExists('src/entry1.test.js');
   assertExists('src/entry2.test.js');
   assertExists('_.._/linked-dependencies/bundled/reporters/default.js');
-  assertExists('bundled_externals/jest-environment-emit/node.js');
-  assertExists('bundled_externals/lodash/noop.js');
+  assertExists('bundled_modules/jest-environment-emit/node.js');
+  assertExists('bundled_modules/lodash/noop.js');
   assertDoesNotExist('node_modules');
   assertDoesNotExist('_.._/linked-dependencies/external');
 }
@@ -39,8 +39,8 @@ function verifyJestConfig() {
   assertEqual(reporters[2][0], '@linked-dependencies/external/reporter', 'reporters[2]');
   assertEqual(reporters[3][0], '<rootDir>/customReporter.js', 'reporters[3]');
   assertEqual(setupFilesAfterEnv.length, 1, 'setupFilesAfterEnv.length');
-  assertEqual(setupFilesAfterEnv[0], '<rootDir>/bundled_externals/lodash/noop.js', 'setupFilesAfterEnv[0]');
-  assertEqual(testEnvironment, '<rootDir>/bundled_externals/jest-environment-emit/node.js', 'testEnvironment');
+  assertEqual(setupFilesAfterEnv[0], '<rootDir>/bundled_modules/lodash/noop.js', 'setupFilesAfterEnv[0]');
+  assertEqual(testEnvironment, '<rootDir>/bundled_modules/jest-environment-emit/node.js', 'testEnvironment');
   assertEqual(testMatch.length, 2, 'testMatch.length');
   assertEqual(testMatch[0], '<rootDir>/src/entry1.test.js', 'testMatch[0]');
   assertEqual(testMatch[1], '<rootDir>/src/entry2.test.js', 'testMatch[1]');
