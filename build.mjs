47c8d5fa6e36d4858262cdb0b6e44bf3cff6f81c
Yaroslav Serhieiev <noomorph@gmail.com>
2025-01-08 13:00:10 +0200

    feat: switch to optional dependencies

--- build.mjs
+++ build.mjs
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 
 import { cosmiconfig } from 'cosmiconfig';
+import { useLoggingForCLI } from './utils/cli-logger.mjs';
 import { logger } from './utils/logger.mjs';
 import { build } from './index.mjs';
 
 try {
+  await useLoggingForCLI();
   const explorer = cosmiconfig('esbuild-jest');
   const esbuildJestBaseConfig = await explorer.search();
   await logger.trace.complete(esbuildJestBaseConfig, 'esbuild-jest-cli', () => build(esbuildJestBaseConfig.config));
